e99168c11037ca2d84d7d0dd2d1224e9a65f9f39
Emaad Ahmed Manzoor <emaadmanzoor@gmail.com>
2014-06-25 00:05:15 +0300

    Authentication operations

--- cli/auth.py
+++ cli/auth.py
@@ -1,25 +1,114 @@
 #!/usr/bin/env python
 
+import click
+import requests
 from igor import igor
+from requests.auth import HTTPBasicAuth
+from requests.exceptions import RequestException
+from netrc_utils import write_credentials, delete_credentials
 
 # Authentication commands
 
 @igor.group()
-def auth():
+@click.pass_obj
+def auth(config):
     """Authentication operations"""
 
 @auth.command()
-def login():
-    """Login and store the authentication token"""
+@click.option('--username', prompt=True)
+@click.option('--password', prompt='Password (typing will be hidden)',
+                            hide_input=True)
+@click.pass_obj
+def login(config, username, password):
+    """Login and obtain an authentication token.
+    
+    This command sends your username/password combination
+    to the Igor API and obtains an authentication token (with an expiry date).
+    This token is stored at ~/.netrc.
+
+    Example of interactive login:
+
+    \b
+    $ igor auth login
+    Enter your credentials:
+    Email: example@osuosl.org
+    Password: (typing will be hidden)
+    Authentication successful.
+
+    Example of parameterized login:
+
+    \b
+    $ igor auth login --username user --password pass
+    Authentication successful.
+    """
+
+    delete_credentials(config.server_host)
+
+    try:
+        response = requests.get(config.server_url + '/login',
+                                auth=(username, password))
+        if response.status_code != requests.codes.ok:
+            print "Authentication failed."
+            exit()
+    except RequestException as error:
+        print 'Error connecting to the Igor API server:', config.server_url
+        print error.message
+        exit()
+
+    token = response.json()['token']
+    config.username = username
+    config.token = token
+    write_credentials(config.server_host, username, token)
+    
+    print "Authentication successful."
 
 @auth.command()
-def logout():
-    """Logout and delete stored tokens"""
+@click.pass_obj
+def logout(config):
+    """Clear local authentication credentials from ~/.netrc.
+    
+    Example:
+    
+    \b
+    $ igor auth logout
+    Local credentials cleared.
+    """
+
+    config.username = None
+    config.token = None
+    delete_credentials(config.server_host)
+    print "Local credentials cleared."
 
 @auth.command()
-def token():
-    """Display the token for the currently logged-in user"""
+@click.pass_obj
+def token(config):
+    """Display the API token for the currently logged-in user.
+    
+    Example:
+    
+    \b
+    $ igor auth token
+    eyJhbGciOiJIUzI1NiIsImV4cCI6MTQwMzY0Mjg3NSwiaWF0IjoxNDAzNjQyMjc1fQ.eyJpZCI6MX0.4pQEJ4zd3e0SudoIumxOeM60uvUbgZtCurS9_0AK4Cg
+    """
+
+    if config.token:
+        print config.token
+    else:
+        print "No logged-in user."
 
 @auth.command()
-def whoami():
-    """Display the username for the currently logged-in user"""
+@click.pass_obj
+def whoami(config):
+    """Display the username for the currently logged-in user.
+    
+    Example:
+
+    \b
+    $ igor auth whoami
+    root
+    """
+
+    if config.username:
+        print config.username
+    else:
+        print "No logged-in user."
--- cli/igor.py
+++ cli/igor.py
@@ -1,21 +1,51 @@
 #!/usr/bin/env python
 
 import click
+import ConfigParser
+from os.path import expanduser
+from netrc_utils import get_credentials
 
 class Config(object):
-    def __init__(self, server, verbose):
+    def __init__(self, server_url, server_host, server_port,
+                 username, token, verbose):
+        self.server_url = server_url
+        self.server_host = server_host
+        self.server_port = server_port
+        self.username = username
+        self.token = token
         self.verbose = verbose
-        self.server = server
-
+        
 @click.group()
 @click.option('--igor-server', envvar='IGOR_SERVER', default='',
-              metavar='URL', help='URL of the Igor API server')
+                               metavar='URL',
+                               help='URL as host:port of the Igor API server')
 @click.option('--verbose', envvar='IGOR_VERBOSE', is_flag=True,
-              default=False, help='Print verbose information')
+                           default=False, help='Print verbose information')
 @click.pass_context
 def igor(ctx, igor_server, verbose):
     """The Igor CLI. Connects to an Igor API server to perform
        IPMI, user and machine management operations.
        
        See 'igor COMMAND --help' for more information on a specific command."""
-    ctx.obj = Config(igor_server, verbose)
+
+    # If not provided, try to read from the config file
+    if not igor_server:
+        config = ConfigParser.RawConfigParser()
+        files = config.read(expanduser('~/.igorrc'))
+        if len(files) > 0:
+            try:
+                igor_server = config.get('igor', 'igor_server')
+            except ConfigParser.Error:
+                pass
+
+    # Store the Igor API server host and port separately for convenience
+    server_url = 'http://' + igor_server
+    server_url_parts = igor_server.split(':')
+    server_host = server_url_parts[0]
+    server_port = server_url_parts[1]
+
+    # Attempt to read login information from ~/.netrc
+    username, token = get_credentials(server_host)
+ 
+    ctx.obj = Config(server_url, server_host, server_port,
+                     username, token, verbose) 
